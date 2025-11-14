0b4f2561d4434bb767fb4b3b89594a9ad5be7919
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-11-14 20:52:02 +0700

    Revert "Merge pull request #1 from Inkubator-IT/feat/blog"
    This reverts commit e7d382090e883599a343aaa9358fbdf96d4b16d2, reversing
    changes made to a11fc3b29707a4b44a5b6bec8eda1e34f96cca3c.

--- src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as BlogListPage } from "./BlogListPage";
-export { default as BlogCreatePage } from "./BlogCreatePage";
-export { default as BlogEditPage } from "./BlogEditPage";
-export { default as TagsPage } from "./TagsPage";
